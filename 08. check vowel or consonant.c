ce69f244c50c08f8d5f17c79fc6e2711d66db6f9
Raisul Islam <shihasraisul12@gmail.com>
2025-10-24 23:51:18 +0600

    Create 08. check vowel or consonant.c

---
+++ 08. check vowel or consonant.c
@@ -0,0 +1,16 @@
+ #include<stdio.h> 
+int main(){ 
+    char ch; 
+    printf("Enter a Alphabets :  "); 
+    scanf("%c",&ch); 
+ 
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+       ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){ 
+            printf("%c is Vowel.", ch); 
+    } 
+    else{ 
+ 
+        printf("%c is Constant.", ch); 
+    } 
+return 0; 
+}
