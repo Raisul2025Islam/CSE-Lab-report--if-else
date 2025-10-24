e7b408eb619130e26939d53043b47db4751995b6
Raisul Islam <shihasraisul12@gmail.com>
2025-10-24 23:42:57 +0600

    Create 01. find maximum between two numbers .c

---
+++ 01. find maximum between two numbers .c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+int main(){  
+    int  Q, R;  
+    printf("Enter value Q :");  
+    scanf("%d",&Q);  
+    printf("Enter value R :");  
+    scanf("%d",&R);  
+    if(Q>R){  
+    printf("Maximum number = %d",Q);  
+        }  
+    else{  
+       printf("Maximum number = %d",R);  
+        } 
+return 0;  
+}
