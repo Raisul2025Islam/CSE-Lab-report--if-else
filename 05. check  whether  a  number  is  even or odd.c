706b62d7d1b03656aafa698f136000a7fe6ab693
Raisul Islam <shihasraisul12@gmail.com>
2025-10-24 23:45:55 +0600

    Create 05. check  whether  a  number  is  even or odd.c

---
+++ 05. check  whether  a  number  is  even or odd.c
@@ -0,0 +1,14 @@
+  #include<stdio.h> 
+int main(){ 
+    int A; 
+    printf("Enter a number:"); 
+    scanf("%d",&A); 
+ 
+    if(A%2==0){ 
+        printf("Number is Even number:"); 
+    } 
+    else{ 
+        printf("Number is Odd number:"); 
+    } 
+return 0; 
+}
